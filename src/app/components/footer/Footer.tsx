a51fe2893901c0f6370c31027fe252c6bdca9bfe
Felipe Torres <fetl9239@gmail.com>
2024-08-03 22:18:22 -0300

    feat: create Footer component and add it to layout.tsx

---
+++ src/app/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./footer.module.css";
+
+const Footer: React.FC = () => {
+  return (
+  <footer className={styles.container}>
+    <p>test</p>
+  </footer>
+  );
+};
+
+export default Footer;
