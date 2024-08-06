5a7c5731d67e76f45b9d4b55ab92f86a6a115484
Felipe Torres <fetl9239@gmail.com>
2024-08-06 20:19:53 -0300

    feat: add svgs as React components (svgr)

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import OpeningHoursSection from "./OpeningHoursSection";
 import PagesSection from "./PagesSection";
 import SafeWebsiteSection from "./SafeWebsiteSection";
 import SocialMediaSection from "./SocialMediaSection";
-import studio1001Image from "@vectors/studio1001.svg";
+import Studio1001 from "@vectors/studio1001.svg";
 import styles from "./footer.module.css";
 
 const Footer: React.FC = () => {
@@ -34,7 +34,7 @@ const BottomSection: React.FC = () => {
     <div className={styles.bottomSection}>
       <div className={styles.createdBy}>
         <p className={typography.caption}>Criado por</p>
-        <img src={studio1001Image.src} width={80} />
+        <Studio1001 />
       </div>
       <p className={classNames(typography.caption, typography.baseline)}>
         Via Midia Comunicação Visual - CNPJ 33.565.366/0001-67
--- src/app/components/footer/SafeWebsiteSection.tsx
+++ src/app/components/footer/SafeWebsiteSection.tsx
@@ -1,12 +1,12 @@
 import typography from "@styles/typography.module.css";
 import styles from "./footer.module.css";
-import GoogleSafeBrowsingImage from "@vectors/GoogleSafeBrowsing.svg";
+import GoogleSafeBrowsing from "@vectors/GoogleSafeBrowsing.svg";
 
 const SafeWebsiteSection: React.FC = () => {
   return (
     <div className={styles.safeWebsiteSection}>
       <p className={typography.subHeader2}>Site Seguro</p>
-      <img src={GoogleSafeBrowsingImage.src} alt="Description of the image" />
+      <GoogleSafeBrowsing />
     </div>
   );
 };
