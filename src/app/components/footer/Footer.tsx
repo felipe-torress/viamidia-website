ec357e4fc3b564d8242984a838c2cbea10f7fa6b
Felipe Torres <fetl9239@gmail.com>
2024-08-06 02:11:13 -0300

    feat: add Studio1001 ogo

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,9 +1,11 @@
+import typography from "@styles/typography.module.css";
+import classNames from "classnames";
 import OpeningHoursSection from "./OpeningHoursSection";
 import PagesSection from "./PagesSection";
 import SafeWebsiteSection from "./SafeWebsiteSection";
 import SocialMediaSection from "./SocialMediaSection";
+import studio1001Image from "@vectors/studio1001.svg";
 import styles from "./footer.module.css";
-import typography from "@styles/typography.module.css";
 
 const Footer: React.FC = () => {
   return (
@@ -30,8 +32,11 @@ const TopSection: React.FC = () => {
 const BottomSection: React.FC = () => {
   return (
     <div className={styles.bottomSection}>
-      <p className={typography.caption}>Criado por Studio 1001</p>
-      <p className={typography.caption}>
+      <div className={styles.createdBy}>
+        <p className={typography.caption}>Criado por</p>
+        <img src={studio1001Image.src} width={80} />
+      </div>
+      <p className={classNames(typography.caption, typography.baseline)}>
         Via Midia Comunicação Visual - CNPJ 33.565.366/0001-67
       </p>
     </div>
