b7ad2674397a0f6fd87c4f357172b63797d53a7f
Felipe Torres <fetl9239@gmail.com>
2024-08-08 01:02:56 -0300

    feat: create colors.ts to handle type-safe colors on app

---
+++ public/colors/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+    white: '#FFFFFF',
+    black: '#000000',
+
+    whatsAppGreen: '#25D366',
+    wazePrimary: '#05C8F7',
+    wazeDarkBlue: '#056A82',
+}
+
+export default colors;
