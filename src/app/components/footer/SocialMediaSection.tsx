93c5414b9ff7f62544f27fa56feb449361b28325
Felipe Torres <85423699+felipe-torress@users.noreply.github.com>
2024-09-22 02:53:01 -0300

    Revert "Revert "Update Typography for Desktop (#5)" (#6)"
    This reverts commit b07373ef59ec9bc1d408b96e0d84db91c4251be1.

--- src/app/components/footer/SocialMediaSection.tsx
+++ src/app/components/footer/SocialMediaSection.tsx
@@ -21,7 +21,7 @@ const SocialMediaSection: React.FC = () => {
           <FaWhatsapp />
         </SocialMediaButton>
       </div>
-      <p className={typography.bodySmall}>
+      <p className={typography.caption}>
         © 2024 - Via Midia Comunicação Visual.
         <br />
         Todos os direitos reservados
