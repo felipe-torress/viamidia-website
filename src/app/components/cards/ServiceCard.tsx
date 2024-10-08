6e126d716c9581639759c9bcc04a2703e094ff44
Felipe Torres <fetl9239@gmail.com>
2024-10-08 03:21:24 -0300

    feat: add responsiveness to ServiceCard component

--- src/app/components/cards/ServiceCard.tsx
+++ src/app/components/cards/ServiceCard.tsx
@@ -1,5 +1,4 @@
 import * as typography from "@styles/typography.module.css";
-import classNames from "classnames";
 import React from "react";
 import * as styles from "./serviceCard.module.css";
 
@@ -17,9 +16,9 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
   contentDescription,
 }) => {
   return (
-    <article className={classNames(styles.cardContainer)}>
+    <article className={styles.cardContainer}>
       <img src={imageSrc} alt={contentDescription} height={200} width={400} />
-      <div className={classNames(styles.cardTextContainer)}>
+      <div className={styles.cardTextContainer}>
         <h3 className={typography.subHeader1}>{title}</h3>
         <Text />
       </div>
